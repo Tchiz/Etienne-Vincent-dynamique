174247c5302be9452ed5be3bf9add68b395be696
Tchi <drugsntchiz@gmail.com>
2013-09-03 19:03:38 +0200

    ajout de deux dates dans l'agenda principal

--- app/views/schedule.php
+++ app/views/schedule.php
@@ -8,4 +8,21 @@
 			</a>
 		</footer>
 	</article>
+	<br/>
+	<article>
+		<header>Le 20 septembre 2013</header>
+		Concert à O'Marchéb'Arts <br/>avec Basile Mouton (cb) et Thomas Delor (dms)
+		<footer>
+			à partir de 20 h au 88 avenue Henri Ginoux, 92120 Montrouge
+		</footer>
+	</article>
+	<br/>
+	<article>
+		<header>Le 30 novembre 2013</header>
+		Concert Swolkin'
+		<footer>
+			à la médiathèque de Montrouge à 15 h
+		</footer>
+	</article>
+	<br/>
 </div>
