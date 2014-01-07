1ab9f4582d8246f619b4da6b523ec79fedb47a9e
Tchi <drugstnchiz@gmail.com>
2014-01-07 22:18:53 +0100

    Ajout des biographies des musiciens d'Etienne Vincent Quartet en dur

--- app/views/vip_musiciens.blade.php
+++ app/views/vip_musiciens.blade.php
@@ -23,8 +23,26 @@
 			alt="No&eacute; Macary - Piano" 
 			title="No&eacute; Macary - Piano" />
 		<p>
-			<a href="./NoeMacary">Voir la page d&eacute;di&eacute;e &agrave; sa biographie</a>
+			<strong>No&eacute; Macary</strong>, pianiste/clavi&eacute;riste, joue dans de nombreuses formations 
+			pour lesquelles il arrange et compose. Son penchant pour les musiques dansantes et 
+			populaires l'a amen&eacute; &agrave; fonder (ou &agrave; se fondre dans...) 
+			plusieurs projets aux <strong>esth&eacute;tiques bien diff&eacute;rentes</strong>, 
+			allant de la pop-rock anglaise des ann&eacute;es 70 (A*Song & the B*sides) au groove des musiques
+			actuelles (Trio Magochi), en passant par la musique traditionnelle réunionnaise 
+			(Fonnzi&eacute;)...et le jazz (Rapha&euml;l Reiter trio), v&eacute;ritable 
+			<strong>racine commune</strong> &agrave; toutes ses recherches stylistiques. 
 		</p>
+		<p>
+			Actuellement &eacute;tudiant en licence au <strong>Conservatoire National 
+			Sup&eacute;rieur de Paris</strong>, No&eacute; multiplieles rencontres musicales. 
+			Il se produit r&eacute;guli&egrave;rement dans la r&eacute;gion lyonnaise : 
+			Sc&egrave;ne de Sibelle <strong>(Jazz &agrave; Vienne)</strong>, le P&eacute;riscope,
+			le Hot-club, 
+			la Clef de Voute, etc, et &eacute;galement en r&eacute;gion parisienne depuis peu: 
+			<strong>Th&eacute;&acirc;tre du Ch&acirc;telet, Th&eacute;&acirc;tre de l’Ouest 
+			Parisien, Th&eacute;&acirc;tre de St Germain-en-Laye, Sunside,etc.</strong>
+		</p>
+
 	</figure>
 	<figure class="musicien">
 		<figcaption>Damien Varaillon - Contrebasse</figcaption>
@@ -33,7 +51,35 @@
 			alt="Damien Varaillon - Contrebasse" 
 			title="Damien Varaillon - Contrebasse" />
 		<p>
-			<a href="./DamienVaraillon">Voir la page d&eacute;di&eacute;e &agrave; sa biographie</a>
+			<strong>Damien Varaillon-Laborie</strong> commence la basse &eacute;lectrique en autodidacte avant 
+			d'entrer au conservatoire de Marseille. A 21 ans, il arr&ecirc;te ses &eacute;tudes 
+			universitaires apr&egrave;s son deug de musicologie pour se consacrer pleinement 
+			&agrave; l'obtention de ses dipl&ocirc;mes d'&eacute;tudes musicales (DEM) en 
+			contrebasse classique, musique de chambre, et contrebasse jazz. Il int&egrave,gre 
+			alors le d&eacute;partement jazz du Conservatoire National Sup&eacute;rieur de 
+			Musique et de Danse de Paris (<strong>CNSM</strong>). 
+		</p>
+		<p>
+			Il se produit d&egrave;s lors dans une 
+			multitudes de contextes: <strong>Musiques symphoniques</strong> avec notamment l'orchestre de Cannes 
+			sous la direction de Philippe Bender, l'op&eacute;ra de Marseille sous la direction 
+			de Jean Claude Casadesus, l'orchestre des jeunes de la M&eacute;diterran&eacute;e 
+			avec Laurent Koercia et Olivier Charlier comme soliste. <strong>Festivals de jazz
+			</strong> (Pinarello
+			,Tanjazz au Maroc, jazz sur la ville, &eacute;clats d' &eacute;mail , Segr&eacute; 
+			jazz festival, Montauban jazz ,festival jazz in Stockholm),en big band avec le Red 
+			Star orchestra et Olivia Ruiz, <strong>tourn&eacute;es en Angleterre</strong>, 
+			en Italie, au Mexique,
+			en Afrique, etc. Clubs de jazz (Sunset , Duc des Lombards , Autours de Midi Minuit,
+			l’improviste ,le pelle m&ecirc;le). 
+		</p>
+		<p>
+			<strong>Il enregistre avec Sylvain Courtney quartet, 
+			Lindau hbf (2&egrave;me prix de groupes a jazz a La D&eacute;fense), Olivier Robin 
+			quintet , Enzo Carniel trio , Mickael Cheret,</strong> etc.
+			Il eu la chance de croiser lors 
+			de master class, Chris Potter, Adam Nussbaum, Larry Grenadier, Billy Hart, Kendrik 
+			Scott, Justin Brown, etc.
 		</p>
 	</figure>
 	<figure class="musicien">
@@ -43,7 +89,25 @@
 			alt="Simon Bernier - Batterie" 
 			title="Simon Bernier - Batterie" />
 		<p>
-			<a href="./SimonBernier">Voir la page d&eacute;di&eacute;e &agrave; sa biographie</a>
+			En Bretagne, <strong>Simon Bernier</strong> grandit dans un environnement musical vari&eacute;. 
+			Apr&egrave;s une ann&eacute;e de piano et une ann&eacute;e de guitare, il trouve sa 
+			voie avec la batterie. Il int&egrave;gre en 2007 la classe de Jazz du conservatoire 
+			de St-Brieuc (22) dirig&eacute;e par Jean-Philippe Lavergne, pendant deux ans. 
+			Il part ensuite pour la capitale et int&egrave;gre la classe Jazz dirig&eacute;e par
+			Jean-Charles Richard au <strong>C.R.R. de Paris</strong>. C'est l'occasion pour lui de multiplier les
+			rencontres avec des musiciens venus de divers horizons. 
+		</p>
+		<p>
+			Actuellement, il est membre de plusieurs formations entre Paris et la Bretagne: 
+			<strong>Ama&ntilde; Octet</strong> (Jazz), Like Jam (Hip-Hop), <strong>Xavier 
+			Lugu&eacute; Trio</strong> en Bretagne, quelques concerts avec <strong>Gildas 
+			Bocl&eacute;, Andr&eacute; Vill&eacute;ger, Gael Horellou, Michel Goldberg, 
+			&Eacute;douard Bineau et Philippe Baden-Powell</strong> &agrave; Paris. Depuis fin 
+			2011, Simon est membre du groupe de <strong>Mina Agossi</strong>. Il a notamment 
+			jou&eacute; au Trianon (Paris), dans les festivals Jazz &agrave; l'Ouest, Crest Jazz
+			Vocal, Jazz &agrave; Vannes, et a remport&eacute; remport&eacute; le 
+			<strong>tremplin Jazz &agrave; St Germain-des-Pr&eacute;s</strong> avec Matteo 
+			Pastorino quartet.
 		</p>
 	</figure>
 </div>
