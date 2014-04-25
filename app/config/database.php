2ff3125ee77d442f37df7944f8b1ebb6f178da86
Tchi <drugstnchiz@gmail.com>
2014-04-25 17:31:10 +0200

    bonne configuration

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'etienne_vincent_database',
-			'username'  => 'etiennev',
-			'password'  => 'Siteweb2014',
+			'database'  => 'database',
+			'username'  => 'root',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
