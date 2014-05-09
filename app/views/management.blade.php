57dc02ff9ec26441831725b96fadab8f240a7997
Tchi <drugstnchiz@gmail.com>
2014-05-09 16:45:54 +0200

    mise en fonctionnement du bouton + de la gestion des biographies

--- app/views/management.blade.php
+++ app/views/management.blade.php
@@ -7,10 +7,10 @@
 	@foreach( $musiciens as $musicien )
 		<div class="item">
 			<div>{{ $musicien->firstname.' '.$musicien->lastname }}</div>
-			<div class="button">+</div>
+			<div class="button"><a href="./editerUneBiographie/{{ $musicien->id }}">+</a></div>
 			<div class="button">-</div>
 		</div>
 	@endforeach
-	<a href="./editerUneBiographie">Ajouter une biographie</a>
+	<a class="button" href="./editerUneBiographie">Ajouter une biographie</a>
 </div>
 @stop
